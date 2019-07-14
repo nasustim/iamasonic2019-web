aee2b0dc13590409b5d2082d91b913407a0ed1d8
nasustim <hibino17@iamas.ac.jp>
2019-07-14 23:55:31 +0900

    :tada: validate font-setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+//const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 const mode = "development";
 const enabledSourceMap = mode === "development"
@@ -50,10 +50,14 @@ module.exports = {
           name: 'res/[name].[ext]'
         }
       },
+      { test: /\.woff$/, loader: 'url?mimetype=application/font-woff&name=[path][name].[ext]' },
+      { test: /\.woff2$/, loader: 'url?mimetype=application/font-woff2&name=[path][name].[ext]' },
+      { test: /\.[ot]tf$/, loader: 'url?mimetype=application/octet-stream&name=[path][name].[ext]' },
+      { test: /\.eot$/, loader: 'url?mimetype=application/vnd.ms-fontobject&name=[path][name].[ext]' }
     ]
   },
   plugins: [
-    new UglifyJSPlugin(),
+    //new UglifyJSPlugin(),
     new HtmlWebpackPlugin({
       filename: './index.html',
       template: './src/index.ejs'
