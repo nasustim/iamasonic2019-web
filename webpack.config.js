55c66e46c701b77fc4f5da27d905d37c5ee0da23
nasustim <hibino17@iamas.ac.jp>
2019-07-15 01:18:24 +0900

    :bug: validate load font-file

--- webpack.config.js
+++ webpack.config.js
@@ -50,11 +50,11 @@ module.exports = {
           name: 'res/[name].[ext]'
         }
       },
-      { test: /\.woff$/, loader: 'url?mimetype=application/font-woff&name=[path][name].[ext]' },
+      /*{ test: /\.woff$/, loader: 'url?mimetype=application/font-woff&name=[path][name].[ext]' },
       { test: /\.woff2$/, loader: 'url?mimetype=application/font-woff2&name=[path][name].[ext]' },
       { test: /\.[ot]tf$/, loader: 'url?mimetype=application/octet-stream&name=[path][name].[ext]' },
       { test: /\.eot$/, loader: 'url?mimetype=application/vnd.ms-fontobject&name=[path][name].[ext]' }
-    ]
+    */]
   },
   plugins: [
     //new UglifyJSPlugin(),
